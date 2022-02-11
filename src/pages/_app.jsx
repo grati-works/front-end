860e2eade98ab52e41f11a4ecb7c019f195e074b
ericknathan <erick.capito@hotmail.com>
2022-02-10 22:27:54 -0300

    Fonte corrigida

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,19 @@
 import '../styles/global.scss';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import { UIProvider } from '../providers/UIProvider';
 
 function MyApp({ Component, pageProps }) {
+  const theme = createTheme({
+    type: "light",
+    theme: {  
+      fonts: {
+        sans: "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;"
+      }
+    }
+  });
+
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <UIProvider>
         <Component {...pageProps} />
       </UIProvider>
