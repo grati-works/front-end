d71565a21db54a6a3ac73d2397b3e3222db318d2
cauaMascena <cauasinho656@gmail.com>
2022-02-14 11:28:16 -0300

    commit 14/02/2022

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -7,24 +7,36 @@ export default function Login() {
       <Head>
           <title>Grati | Login</title>
       </Head>
-      <div className={styles.divImgFundo}>
-        <img src="/images/imgFundoLogin.jpg" alt="imagemDeFundo" className={styles.imgFundo}/>
+      <div className={styles.ImgFundo}>
+        <img src="/images/imgFundoLogin.jpg" alt="imagemDeFundo"/>
       </div>
       <div className={styles.containerLogin}>
         <img src="/images/Logo1.png" alt="logoGrati" />
         <h1>Login</h1>
 
         <div className={styles.inputs}>
-        <img src="/images/user.png" alt="pessoa" />
-        <input id='user' type="text" placeholder='Usuário'/>
+
+          <div className={styles.containerInput}>
+            <div className={styles.imgUser}>
+               <img src="/images/user.png" alt="pessoa" />
+            </div>
+            <input id='user' type="text" placeholder='Usuário'/>
+          </div>
+
+        <div className={styles.containerInput}>
+        <div className={styles.imgPassword}>
         <img src="/images/password.png" alt="cadeado" />
+        </div>
         <input id='password' type="text" placeholder='Senha'/>
         </div>
-        
-        <button>Autenticar</button>
-        <a href="">Esqueci minha senha</a>
+        </div>
+
+        <div className={styles.esqueciminhaSenha}>
+        <button className={styles.button1}>Autenticar</button>
+        <a href="#">Esqueci minha senha</a>
+        </div>
         <h2>Não possui conta?</h2>
-        <button>Cadastra-se</button>
+        <button className={styles.button2}>Cadastra-se</button>
       </div>
     </>
   )
