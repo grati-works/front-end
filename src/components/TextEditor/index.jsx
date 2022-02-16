17fb4dc582c270740bc8adee39463b10dafa7c8d
ericknathan <erick.capito@hotmail.com>
2022-02-16 11:08:38 -0300

    Editor de texto finalizado

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Card, Divider, Button, Tooltip } from '@nextui-org/react';
+import { useEffect, useRef, useState } from 'react'
+import { Card, Divider, Button, Tooltip, Input } from '@nextui-org/react';
 import styles from './styles.module.scss';
 import { Heart, ChevronDown } from 'react-iconly';
 import { Picker } from 'emoji-mart'
@@ -36,23 +36,92 @@ export function TextEditor() {
     },
   };
 
+  const inputFile = useRef(null);
+  const [attached, setAttached] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [selectedPrivacy, setSelectedPrivacy] = useState("public");
+  const [newTagText, setNewTagText] = useState("");
+  const [newUserText, setNewUserText] = useState("");
+  const [data, setData] = useState({
+    users: [
+      "ericknathan",
+      "pedrothecatholic"
+    ],
+    tags: [
+      "Resiliência",
+    ]
+  });
+
+  function handleDeleteUser(id) {
+    const newUsers = data.users.filter((_, index) => index !== id);
+    setData({ ...data, users: newUsers });
+  }
+
+  function handleDeleteTag(id) {
+    const newTags = data.tags.filter((_, index) => index !== id);
+    setData({ ...data, tags: newTags });
+  }
+
+  function handleAddTag(name) {
+    setData({ ...data, tags: [...data.tags, name] });
+  }
+
+  function handleAddUser(name) {
+    setData({ ...data, users: [...data.users, name] });
+  }
+
+  useEffect(() => {
+    const file = inputFile.current.files[0];
+    const fileReader = new FileReader();
+
+    if (file) {
+      fileReader.readAsDataURL(file)
+    } else {
+      setImagePreview('../images/more-icon.png');
+    }
+
+    fileReader.onloadend = () => setImagePreview(fileReader.result);
+ }, [attached])
+
   return (
     <Card className={styles.textEditorContainer} shadow={false}>
       <Card.Header className={styles.header}>
         <div className={styles.users}>
-          <button className={styles.user}>
-            <p>@ericknathan</p>
-          </button>
-          <button className={styles.user}>
-            <p>@pedrothecatholic</p>
-          </button>
-          <input type="text" className={styles.userInput}/>
+          {
+            data.users.map((user, id) => (
+              <button className={styles.user} key={id} onClick={() => handleDeleteUser(id)}>
+                <p>@{user}</p>
+              </button>
+            ))
+          }
+          <input
+            type="text"
+            className={styles.userInput}
+            placeholder="Insira o usuário"
+            value={newUserText}
+            onChange={input => setNewUserText(input.target.value)}
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                handleAddUser(newUserText);
+                setNewUserText('');
+              }
+            }}
+          />
         </div>
         <Tooltip placement="bottom" color="transparent" shadow={false} trigger="click" content={
           <Button.Group size="lg" vertical >
-            <Button>Público</Button>
-            <Button ghost>Privado</Button>
-            <Button ghost>Grupo</Button>
+            <Button
+              ghost={selectedPrivacy !== "public" ? true : false}
+              onClick={() => setSelectedPrivacy("public")}
+            >Público</Button>
+            <Button
+              ghost={selectedPrivacy !== "private" ? true : false}
+              onClick={() => setSelectedPrivacy("private")}
+            >Privado</Button>
+            <Button
+              ghost={selectedPrivacy !== "group" ? true : false}
+              onClick={() => setSelectedPrivacy("group")}
+            >Grupo</Button>
           </Button.Group>
         }>
           <button className={styles.privacy}>
@@ -63,20 +132,49 @@ export function TextEditor() {
       <Divider/>
       <Card.Body>
         <div className={styles.tags}>
-          <button className={styles.tag}>
-            <div className={styles.icon}>
-              <Heart />
-            </div>
-            <p>Resiliência</p>
-          </button>
-          <button className={styles.tag}>
-            <ChevronDown className={styles.addIcon}/>
-          </button>
+          {
+            data.tags.map((tag, id) => (
+              <button className={styles.tag} key={id} onClick={() => handleDeleteTag(id)}>
+                <div className={styles.icon}>
+                  <Heart />
+                </div>
+                <p>{tag}</p>
+              </button>
+            ))
+          }
+          <Tooltip placement="bottom" trigger="click" content={
+            <Input
+              className={styles.tagInput}
+              placeholder="Insira a tag"
+              value={newTagText}
+              onChange={input => setNewTagText(input.target.value)}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  handleAddTag(newTagText);
+                  setNewTagText('');
+                }
+              }}
+            />
+          }>
+            <button className={styles.tag}>
+              <ChevronDown className={styles.addIcon}/>
+            </button>
+          </Tooltip>
         </div>
         <textarea
           placeholder="Digite aqui sua mensagem..."
           className={styles.input}
         ></textarea>
+        <Button
+          className={attached ? styles.inputFileVisible : styles.inputFile}
+          color="background"
+          onClick={() => {
+            setAttached(false)
+          }}
+        >
+          <img src={imagePreview} alt="" />
+        </Button>
+        <input type='file' id='file' ref={inputFile} onChange={() => setAttached(true)} style={{ display: 'none' }} />
       </Card.Body>
       <Divider/>
       <Card.Footer>
@@ -94,7 +192,11 @@ export function TextEditor() {
               <img src="/icons/emoji.svg" alt="" />
             </Button>
           </Tooltip>
-          <Button className={styles.attachment} auto>
+          <Button className={styles.attachment} auto onClick={() => {
+            inputFile.current.value = '';
+            inputFile.current.click();
+            setAttached(false)
+          }}>
             <img src="/icons/image.svg" alt="" />
           </Button>
           <Tooltip placement="right" trigger="click" content={
@@ -113,9 +215,6 @@ export function TextEditor() {
               <img src="/icons/gif.svg" alt="" />
             </Button>
           </Tooltip>
-          <Button className={styles.attachment} auto>
-            <img src="/icons/document.svg" alt="" />
-          </Button>
         </div>
         <Button className={styles.sendGratiButton}>Enviar grati</Button>
       </Card.Footer>
