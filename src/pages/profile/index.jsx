d4275d9ac4524d31b84b5e3f3c5fb959da521e5a
cauaMascena <cauasinho656@gmail.com>
2022-02-18 11:29:21 -0300

    atualizacoes

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -8,6 +8,18 @@ export default function Profile() {
       <Head>
           <title>Grati | Perfil</title>
       </Head>
+
+      <div className={styles.containerNavBar}>
+        <div className={styles.ImgIcons}>
+          <nav><img src="/images/imgGitHub.png" alt="imgGitHub"/>Github</nav>
+          <nav><img src="/images/imgLinkedin.png" alt="imgLinkedin"/>Linkedin</nav>
+          <nav><img src="/images/imgDribbble.png" alt="imgDribbble"/>Dribbble</nav>
+
+        </div>
+        <div className={styles.imgPerfil}>
+          <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" alt="imgPerfil" />
+        </div>
+      </div>
     </>
   )
 }
