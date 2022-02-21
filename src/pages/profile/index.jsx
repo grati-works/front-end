cd0a7ba45be0092cdb40f7edbe198089c9d15603
cauaMascena <cauasinho656@gmail.com>
2022-02-21 11:27:22 -0300

    desenvolvimento pagina de perfil

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -10,14 +10,27 @@ export default function Profile() {
       </Head>
 
       <div className={styles.containerNavBar}>
-        <div className={styles.ImgIcons}>
-          <nav><img src="/images/imgGitHub.png" alt="imgGitHub"/>Github</nav>
-          <nav><img src="/images/imgLinkedin.png" alt="imgLinkedin"/>Linkedin</nav>
-          <nav><img src="/images/imgDribbble.png" alt="imgDribbble"/>Dribbble</nav>
-
-        </div>
+        <img className={styles.imgFundo} src="/images/imgFundoProfile.png" alt="imagemDeFundo" />
         <div className={styles.imgPerfil}>
           <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" alt="imgPerfil" />
+        </div>
+        <dir className={styles.userName}>
+          <h1>Túlio Nogueira</h1>
+          <h2>Product Owner</h2>
+        </dir>
+        <div className={styles.imgIcons}>
+          <div><img src="/images/imgGitHub.png" alt="imgGitHub"/>Github</div>
+          <div><img src="/images/imgLinkedin.png" alt="imgLinkedin"/>Linkedin</div>
+          <div><img src="/images/imgDribbble.png" alt="imgDribbble"/>Dribbble</div>
+        </div>
+      </div>
+
+      <div className={styles.conteudoBox}>
+        <div className={styles.lateralEsquerda}>
+
+        </div>
+        <div className={styles.lateralDireita}>
+          
         </div>
       </div>
     </>
