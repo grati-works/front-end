33db81a1caa95e8d3e80bd9779ccd09bf94895f8
cauaMascena <cauasinho656@gmail.com>
2022-03-04 11:25:13 -0300

    lateral direita pagina home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,11 +16,19 @@ export default function HomeUser() {
                 <div className={styles.feed}>
                     <GratiCard />
                     <GratiCard />
+                    <GratiCard />
+                    <GratiCard />
                 </div>
             </div>
             <div className={styles.info}>
-                <div className={styles.points}></div>
-                <div className={styles.experience}></div>
+                <div className={styles.points}>
+                  <img src="/images/imgXp.png" alt="Xp" />
+                  <p>Você acumulou 26000 xp durante esse mes.</p>
+                </div>
+                <div className={styles.experience}>
+                  <img src="/images/imgXp.png" alt="Xp" />
+                  <p>Uma meta de 5300 xp está agendada para 16/02.</p>
+                </div>
                 <div className={styles.ranking}></div>
             </div>
         </div>
