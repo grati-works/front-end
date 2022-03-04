5dc698613de897c1ab7588223d6df75b303f8500
ericknathan <erick.capito@hotmail.com>
2022-03-03 22:23:50 -0300

    Cards de preço adicionado

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './landingPage.module.scss';
+import { Button } from '../components/Button';
 
 export default function Home() {
   return (
@@ -12,12 +13,12 @@ export default function Home() {
           <meta property="og:url" content="https://grati.works/" />
           <meta property="og:type" content="website" />
       </Head>
-      <>
+      <main className={styles.landingPageWrapper}>
         <img src="/images/circle.png" alt="circle1" className={styles.circle1} />
         <img src="/images/circle.png" alt="circle2" className={styles.circle2} />
         <img src="/images/circle.png" alt="circle3" className={styles.circle3} />
 
-        <div className={styles.text}>
+        <div className={styles.mainSectionContainer}>
           <h1>Adote <span>feedbacks</span> em sua organização</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci dui sed amet elementum, nunc augue mauris gravida ultrices.</p>
           <div className={styles.adquireButtons}>
@@ -27,16 +28,47 @@ export default function Home() {
         <img src="/images/mockups.png" alt="Aplicativos mobile" /> 
         </div>
 
-        <div className={styles.containerPlanos}>
-          <h1>Planos</h1>
-          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor sem odio ut viverra mi quam.</h2>
-          <button><a href="#">Plano Mensal</a></button>
-          <button><a href="#">Plano Anual</a></button>
+        <div className={styles.plansSectionContainer}>
+          <h2>Planos</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor sem odio ut viverra mi quam.</p>
+          <div className={styles.planFilter}>
+            <button className={styles.active}>Planos Mensais</button>
+            <button>Planos Anuais</button>
+          </div>
+          <div className={styles.planList}>
+            <div className={styles.plan}>
+              <h3>Plano Mensal</h3>
+              <p>All the basics for starting a new business</p>
+              <div className={styles.planPrice}>
+                R$XX
+                <span>/mês</span>
+              </div>
+              <Button>Adquirir plano</Button>
+            </div>
+            <div className={styles.plan}>
+              <h3>Plano Trimestral</h3>
+              <p>All the basics for starting a new business</p>
+              <div className={styles.planPrice}>
+                R$XX
+                <span>/mês</span>
+              </div>
+              <Button>Adquirir plano</Button>
+            </div>
+            <div className={styles.plan}>
+              <h3>Plano Semestral</h3>
+              <p>All the basics for starting a new business</p>
+              <div className={styles.planPrice}>
+                R$XX
+                <span>/mês</span>
+              </div>
+              <Button>Adquirir plano</Button>
+            </div>
+          </div>
         </div>
 
-        <div className={styles.containerSobre}>
-          <h1>Sobre nós</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        <div className={styles.aboutSectionContainer}>
+          <h2>Sobre nós</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Dolor sem odio ut viverra mi quam. Vitae fames porttitor arcu tempor vulputate proin. 
             Vitae erat a, vitae a amet vestibulum. Diam a etiam ipsum quis auctor massa. 
             Lacinia at amet facilisi mattis massa placerat. 
@@ -47,9 +79,9 @@ export default function Home() {
             Placerat duis nunc molestie dui mi. Velit, aliquam cum faucibus at maecenas at auctor ut in. 
             Neque urna cum ultrices vitae odio etiam quam sed nunc. 
             Felis rhoncus at gravida condimentum viverra quis id bibendum sollicitudin.
-            </p>
+          </p>
         </div>
-      </>
+      </main>
     </>
   )
 }
