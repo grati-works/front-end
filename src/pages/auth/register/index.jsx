22f333e7bc17c9f7d3bb7154493d247efa4bc16a
cauaMascena <cauasinho656@gmail.com>
2022-03-07 11:28:07 -0300

    pre-finalizacao da pagina de recuperacao de senha e inicio da pagina de cadastro

--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -1,14 +1,14 @@
 import Head from 'next/head';
-import styles from './recuperSenha.module.scss';
+import styles from './cadastro.module.scss';
 import { Input } from '../../../components/Input';
 import { User, Lock } from 'react-iconly';
 import { Button } from '../../../components/Button';
 
-export default function RecoverPassword() {
+export default function Register() {
   return (
     <div className={styles.containerLogin}>
       <Head>
-          <title>Grati | Recuperação de Senha</title>
+          <title>Grati | Cadastro</title>
           <meta name="description" content="Realizar login" />
           <meta property="og:title" content="Login" />
           <meta property="og:url" content="https://grati.works/auth/signin" />
@@ -17,9 +17,12 @@ export default function RecoverPassword() {
       <img src="/images/auth_background.png" alt="Ilustração de autenticação" className={styles.ImgFundo}/>
       <div className={styles.conteudo}>
         <img src="/images/logo_dark.svg" alt="Logo Grati" />
-        <h1>Recuperar Senha</h1>
+        <h1>Cadastro</h1>
         <div className={styles.inputs}>
-          <Input Icon={Lock} placeholder="E-mail" />
+            <Input Icon={Lock} placeholder="E-mail" />
+            <Input Icon={User} placeholder="Nome" />
+            <Input Icon={User} placeholder="Usuário" />
+            <Input Icon={Lock} placeholder="Senha" />
         </div>
         <Button>Autenticar</Button>
       </div>
