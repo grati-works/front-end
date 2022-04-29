50693d11f0ef8730f8aef83106840dc6e8590536
cauaMascena <cauasinho656@gmail.com>
2022-04-29 11:22:53 -0300

    pagina de faq criada

---
+++ src/pages/faq/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./faq.module.scss";
+
+export default function Faq() {
+  
+  return (
+    <div className={styles.groupCenter}>
+      <div className={styles.containerLeft}>
+          <img src="./images/faqBackground.jpg" alt="background" />
+      </div>
+      <div className={styles.containerFaq}>
+
+      </div>
+    </div>
+  );
+}
