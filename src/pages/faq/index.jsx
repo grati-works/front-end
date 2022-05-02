a17f47656eb692de4918b6c24a4f09a10e1b531f
cauaMascena <cauasinho656@gmail.com>
2022-05-02 11:02:12 -0300

    modal faq

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,15 +1,55 @@
 import styles from "./faq.module.scss";
+import { useRef, useState } from "react";
+import { Modal } from "@nextui-org/react";
+import { Category } from "react-iconly";
+import { Input } from "../../components/Input";
 
 export default function Faq() {
-  
+  const inputFile = useRef(null);
+  const [visibleInfo, setVisibleInfo] = useState(false);
+  const handlerInfo = () => setVisibleInfo(true);
+  const closeHandlerInfo = () => {
+    setVisibleInfo(false);
+  };
   return (
+    <>
     <div className={styles.groupCenter}>
       <div className={styles.containerLeft}>
           <img src="./images/faqBackground.jpg" alt="background" />
       </div>
       <div className={styles.containerFaq}>
-
+        <div className={styles.boxQuestions}>
+          <h1>What this?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci dui sed amet elementum, nunc augue mauris gravida ultrices.</p>
+        </div>
+        <div className={styles.boxQuestions}>
+          <h1>What is it for?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci dui sed amet elementum, nunc augue mauris gravida ultrices.</p>
+        </div>
+        <div className={styles.boxQuestions}>
+          <h1>How do I get this application?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci dui sed amet elementum, nunc augue mauris gravida ultrices.</p>
+        </div>
+        <div className={styles.moreQuestion} onClick={handlerInfo}>
+          +
+        </div>
       </div>
     </div>
+    <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visibleInfo}
+        onClose={closeHandlerInfo}
+        className={styles.groupsModal}
+        width="530px"
+        scroll
+      >
+        <Modal.Header className={styles.groupsModalHeader}>
+        </Modal.Header>
+        <Modal.Body className={styles.groupsWrapper}>
+          <Input Icon={Category} placeholder="Digite sua pergunta"/>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
